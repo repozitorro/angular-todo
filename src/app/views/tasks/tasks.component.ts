ba7ce4dbf8e065f17e2c39c0bdd679b4314ed4fd
Bogdan <repozitorro@gmail.com>
2020-08-16 19:02:34 +0300

    Add: Material Design styled data-table

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DataHandlerService} from "../../service/data-handler.service";
 import {Task} from 'src/app/model/Task';
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
     selector: 'app-tasks',
@@ -9,19 +10,49 @@ import {Task} from 'src/app/model/Task';
 })
 export class TasksComponent implements OnInit {
 
+    // поля для таблицы (те, что отображают данные из задачи - должны совпадать с названиями переменных класса)
+    private displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+    private dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
+
     tasks: Task[];
 
     constructor(private dataHandler: DataHandlerService) {
     }
 
     ngOnInit() {
-        // this.tasks = this.dataHandler.getTasks();
-        // stream rxjs
-        this.dataHandler.taskSubject.subscribe(task => this.tasks = task)
+        this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
 
+        // датасорс обязательно нужно создавать для таблицы, в него присваивается любой источник (БД, массивы, JSON и пр.)
+        this.dataSource = new MatTableDataSource();
+
+        this.refreshTable();
     }
 
     toggleTaskCompleted(task: Task) {
         task.completed = !task.completed
+    }
+
+    // в зависимости от статуса задачи - вернуть цвет названия
+    private getPriorityColor(task: Task) {
+
+        // цвет завершенной задачи
+        if (task.completed) {
+            return '#F8F9FA';
+        }
+
+        if (task.priority && task.priority.color) {
+            return task.priority.color;
+        }
+
+        return '#fff';
+
+    }
+
+    // показывает задачи с применением всех текущий условий (категория, поиск, фильтры и пр.)
+    private refreshTable() {
+
+        this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
+
+
     }
 }
